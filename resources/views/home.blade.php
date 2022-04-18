a39ddbe01ffb0f3366c4ed3692958953cbedc6e9
Rosario Ramirez Rios <rrrosario16@gmail.com>
2022-04-18 13:46:07 -0500

    CAMBIO VISTA PRINICIPAL

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,5 @@
 @section('title', 'Home')
 
 @section('content')
-    <h1 class="mt-5 mb-5">Bienvenido a la página principal</h1>
+    <h1 class="mt-5 mb-5">Bienvenido a la página principal BLOG 2022</h1>
 @endsection
